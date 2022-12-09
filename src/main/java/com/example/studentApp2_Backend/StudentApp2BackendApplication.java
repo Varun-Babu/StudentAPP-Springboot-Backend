97e55e219acb340f9502ea29d30d4dc7469793ca
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-09 15:00:13 +0530

    initial commit

---
+++ src/main/java/com/example/studentApp2_Backend/StudentApp2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentApp2_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApp2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApp2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentApp2_Backend/StudentApp2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentApp2_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApp2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
